90fd0a18bccb120a7508f594441f2eea29b2db10
Kevin Lubick <kjlubick@ncsu.edu>
2015-01-24 09:31:22 -0500

    Sending email works!
    I had to verify my email first, but now an email can be sent :+1:
    
    Code is from the aws examples AmazonSESSample.java

---
+++ src/main/java/edu/kjlubick/ncsu/SendAnEmail.java
@@ -0,0 +1,76 @@
+package edu.kjlubick.ncsu;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClient;
+import com.amazonaws.services.simpleemail.model.Body;
+import com.amazonaws.services.simpleemail.model.Content;
+import com.amazonaws.services.simpleemail.model.Destination;
+import com.amazonaws.services.simpleemail.model.Message;
+import com.amazonaws.services.simpleemail.model.SendEmailRequest;
+
+public class SendAnEmail {
+
+	//from the AWS SAMPLE AMAZON SESSAMPLE
+	public static void main(String[] args) {
+		 // Construct an object to contain the recipient address.
+        Destination destination = new Destination().withToAddresses(new String[]{"kjlubick@ncsu.edu"});
+
+        // Create the subject and body of the message.
+        Content subject = new Content().withData("Hello World");
+        Content textBody = new Content().withData("This is a test of the aws system");
+        Body body = new Body().withText(textBody);
+
+        // Create a message with the specified subject and body.
+        Message message = new Message().withSubject(subject).withBody(body);
+
+        // Assemble the email.
+        SendEmailRequest request = new SendEmailRequest().withSource("kjlubick@ncsu.edu").withDestination(destination).withMessage(message);
+
+        try {
+            System.out.println("Attempting to send an email through Amazon SES by using the AWS SDK for Java...");
+
+            /*
+             * The ProfileCredentialsProvider will return your [default]
+             * credential profile by reading from the credentials file located at
+             * (~/.aws/credentials).
+             *
+             * TransferManager manages a pool of threads, so we create a
+             * single instance and share it throughout our application.
+             */
+            AWSCredentials credentials = null;
+            try {
+                credentials = new ProfileCredentialsProvider().getCredentials();
+            } catch (Exception e) {
+                throw new AmazonClientException(
+                        "Cannot load the credentials from the credential profiles file. " +
+                        "Please make sure that your credentials file is at the correct " +
+                        "location (~/.aws/credentials), and is in valid format.",
+                        e);
+            }
+
+            // Instantiate an Amazon SES client, which will make the service call with the supplied AWS credentials.
+            AmazonSimpleEmailServiceClient client = new AmazonSimpleEmailServiceClient(credentials);
+
+            // Choose the AWS region of the Amazon SES endpoint you want to connect to. Note that your production
+            // access status, sending limits, and Amazon SES identity-related settings are specific to a given
+            // AWS region, so be sure to select an AWS region in which you set up Amazon SES. Here, we are using
+            // the US East (N. Virginia) region. Examples of other regions that Amazon SES supports are US_WEST_2
+            // and EU_WEST_1. For a complete list, see http://docs.aws.amazon.com/ses/latest/DeveloperGuide/regions.html
+            Region REGION = Region.getRegion(Regions.US_WEST_2);
+            client.setRegion(REGION);
+
+            // Send the email.
+            client.sendEmail(request);
+            System.out.println("Email sent!");
+
+        } catch (Exception ex) {
+            System.out.println("The email was not sent.");
+            System.out.println("Error message: " + ex.getMessage());
+        }
+	}
+
+}
